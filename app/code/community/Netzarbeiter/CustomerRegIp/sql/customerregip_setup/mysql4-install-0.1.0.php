570ca0e424b070ec291fa92055cb747f3e9a74b4
Vinai Kopp <vinai@netzarbeiter.com>
2014-03-20 08:35:37 +0100

    Fix type hint

--- app/code/community/Netzarbeiter/CustomerRegIp/sql/customerregip_setup/mysql4-install-0.1.0.php
+++ app/code/community/Netzarbeiter/CustomerRegIp/sql/customerregip_setup/mysql4-install-0.1.0.php
@@ -17,7 +17,7 @@
  * license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-/* @var $installer Mage_Eav_Model_Entity_Setup */
+/* @var $installer Mage_Customer_Model_Resource_Setup */
 $installer = $this;
 
 $installer->startSetup();
